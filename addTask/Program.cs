3050caeddc85286398f870139e9540c41b0f0855
malov.alexsander <malov.alexsander@yandex.ru>
2017-11-30 14:32:14 +0600

    metod SendMessage

--- addTask/Program.cs
+++ addTask/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 
 namespace addTask
 {
@@ -11,34 +12,55 @@ namespace addTask
         static void Main(string[] args)
         {
             int update_id = 0;
+            string message_id = "";
+            string id_user = "";
+            string id_chat= "";
 
-            string url = "https://api.telegram.org/bot";
+            string text_user = "";
+
+
+            string urlBot = "https://api.telegram.org/bot";
             string token = "487607339:AAHr8V9HG_SznrwE_3eRbGnkpKSfVCNKj1s";
 
             WebClient webClient = new WebClient();
 
-            String urlGetMe = $"{url}{token}/getMe";
+            String urlGetMe = $"{urlBot}{token}/getMe";
 
 
             while (true)
             {
-                String urlUpdate = $"{url}{token}/getUpdates?offset={update_id + 1}";
-                String req = webClient.DownloadString(urlUpdate);
+                String url = $"{urlBot}{token}/getUpdates?offset={update_id + 1}";
+                String req = webClient.DownloadString(url);
 
                 var array = Newtonsoft.Json.Linq.JObject.Parse(req)["result"].ToArray();
 
                 for (int i = 0; i < array.Length; i++)
                 {
-                    //Console.WriteLine($"{array[i]["message"]["from"].ToString()}");
-                    Console.WriteLine($"{array[i].ToString()}");
+                    String messegeSendText = "www";
 
+                    update_id = Convert.ToInt32(array[i]["update_id"]);
+                    message_id = Convert.ToString(array[i]["message"]["message_id"]);
+                    id_user = Convert.ToString(array[i]["message"]["from"]["id"]);
+                    id_chat = Convert.ToString(array[i]["message"]["chat"]["id"]);
+                    text_user = Convert.ToString(array[i]["message"]["text"]);
 
+                    try
+                    {
+                        Console.WriteLine(message_id);
+                        Console.WriteLine(update_id);
+                        Console.WriteLine(id_user);
+                        Console.WriteLine(id_chat);
+                        Console.WriteLine(text_user);
+
+                        url = $"{urlBot}{token}/sendMessage?chat_id={id_chat}&text={messegeSendText}";
+                        webClient.DownloadString(url);
+                    }
+                    catch 
+                    {
+                    }
                 }
-                Console.ReadLine();
+                Thread.Sleep(1000);
             }
-
-            
-
         }
     }
 }
