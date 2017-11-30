51d3eb074c4cf72b66248102808264baa6c3109e
malov.alexsander <malov.alexsander@yandex.ru>
2017-11-30 19:55:08 +0300

    my id user >> task

--- addTask/Program.cs
+++ addTask/Program.cs
@@ -13,7 +13,7 @@ namespace addTask
         {
             int update_id = 0;
             string message_id = "";
-            string id_user = "";
+            int id_user;
             string id_chat= "";
 
             string text_user = "";
@@ -26,7 +26,7 @@ namespace addTask
 
             String urlGetMe = $"{urlBot}{token}/getMe";
 
-
+            
             while (true)
             {
                 String url = $"{urlBot}{token}/getUpdates?offset={update_id + 1}";
@@ -36,11 +36,11 @@ namespace addTask
 
                 for (int i = 0; i < array.Length; i++)
                 {
-                    String messegeSendText = "www";
+                    String messegeSendText = "";
 
                     update_id = Convert.ToInt32(array[i]["update_id"]);
                     message_id = Convert.ToString(array[i]["message"]["message_id"]);
-                    id_user = Convert.ToString(array[i]["message"]["from"]["id"]);
+                    id_user = Convert.ToInt32(array[i]["message"]["from"]["id"]);
                     id_chat = Convert.ToString(array[i]["message"]["chat"]["id"]);
                     text_user = Convert.ToString(array[i]["message"]["text"]);
 
@@ -52,8 +52,19 @@ namespace addTask
                         Console.WriteLine(id_chat);
                         Console.WriteLine(text_user);
 
-                        url = $"{urlBot}{token}/sendMessage?chat_id={id_chat}&text={messegeSendText}";
-                        webClient.DownloadString(url);
+                        if ( id_user == 130116992) {
+                            messegeSendText = Console.ReadLine();
+
+                            url = $"{urlBot}{token}/sendMessage?chat_id={id_chat}&text={messegeSendText}";
+                            webClient.DownloadString(url);
+                        }
+                        else
+                        {
+                            messegeSendText = "Who are you?";
+
+                            url = $"{urlBot}{token}/sendMessage?chat_id={id_chat}&text={messegeSendText}";
+                            webClient.DownloadString(url);
+                        }                        
                     }
                     catch 
                     {
